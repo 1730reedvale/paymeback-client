c16f314675c209a9ce95df89a62cf69bb60b150a
1730reedvale <1730reedvale@gmail.com>
2025-10-11 17:26:11 -0700

    Log VITE_API_BASE for debugging

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+console.log("VITE_API_BASE =", import.meta.env.VITE_API_BASE);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
